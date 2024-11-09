ae8f01eefc91baf5ccdeb1afa38ad78f46f9b9b4
SHREYANSH <137887705+02shreyansh@users.noreply.github.com>
2024-11-10 01:55:49 +0530

    Update API.jsx

--- src/redux/API.jsx
+++ src/redux/API.jsx
@@ -1,4 +1,7 @@
-export const HUGGING_API_URL='https://api-inference.huggingface.co/models/meta-llama/Llama-3.2-11B-Vision-Instruct/v1/chat/completions'
-export const HUGGING_API_KEY='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'
-export const STABLE_DIFFUSION_KEY='NKxdqncXIqazMBsUw6P5cq4IlQnYIRNxqRI5HewPFxbjRbRvZduLraCJFpZL'
-export const STABLE_DIFFUSION_URL='https://modelslab.com/api/v6/realtime/text2img'
+Replace placeholders with your credentials.
+Update the "model" in the fetchResponse function within the SendButton component located at /components/SendButton.jsx.
+
+export const HUGGING_API_URL=''
+export const HUGGING_API_KEY=''
+export const STABLE_DIFFUSION_KEY=''
+export const STABLE_DIFFUSION_URL=''
