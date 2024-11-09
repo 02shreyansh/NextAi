7a47b721321233098f3c84cd19ea54123e6b6e50
SHREYANSH <137887705+02shreyansh@users.noreply.github.com>
2024-11-10 01:56:07 +0530

    Update API.jsx

--- src/redux/API.jsx
+++ src/redux/API.jsx
@@ -1,5 +1,5 @@
-Replace placeholders with your credentials.
-Update the "model" in the fetchResponse function within the SendButton component located at /components/SendButton.jsx.
+// Replace placeholders with your credentials.
+// Update the "model" in the fetchResponse function within the SendButton component located at /components/SendButton.jsx.
 
 export const HUGGING_API_URL=''
 export const HUGGING_API_KEY=''
